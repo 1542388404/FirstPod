89c0110f80be4f2f63323fcf152a3ae4cf0fb70e
ahao <ahao@ahaodeMacBook-Pro.local>
2020-08-17 15:26:00 +0800

    1.开始添加库，看配置的spec中的包含和排除是否生效

---
+++ FirstPodFramework/FirstPodFramework/FirstPodController.h
@@ -0,0 +1,17 @@
+//
+//  FirstPodController.h
+//  FirstPodFramework
+//
+//  Created by ahao on 2020/8/17.
+//  Copyright © 2020 ahao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FirstPodProtocol.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstPodController : NSObject<FirstPodProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
