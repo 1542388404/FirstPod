484908750588c104562597f4070064b4249ab1db
ahao <ahao@ahaodeMacBook-Pro.local>
2020-08-17 13:47:31 +0800

    1.测试pod

---
+++ FirstPodFramework/FirstPodFramework/FirstPodFramework.h
@@ -0,0 +1,19 @@
+//
+//  FirstPodFramework.h
+//  FirstPodFramework
+//
+//  Created by ahao on 2020/8/17.
+//  Copyright © 2020 ahao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstPodFramework.
+FOUNDATION_EXPORT double FirstPodFrameworkVersionNumber;
+
+//! Project version string for FirstPodFramework.
+FOUNDATION_EXPORT const unsigned char FirstPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstPodFramework/PublicHeader.h>
+
+
